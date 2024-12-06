98831826b1deae59ca185669bff097c80680b5c2
russerr <mediano_78_45@live.com>
2024-12-06 01:28:00 -0500

    init commit

---
+++ src/test/java/pe/edu/cibertec/inventario/InventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
